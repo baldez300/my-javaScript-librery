2715d1b7c284355c24344a4b32862a0ddd2321ee
baldez300 <81166801+baldez300@users.noreply.github.com>
2022-08-17 14:03:18 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+﻿self.addEventListener('install', e => {
+    e.waitUntil(
+      caches.open('video-store').then(function(cache) {
+        return cache.addAll([
+          '/Users/baldez/Desktop/JavaScript-examples/',
+          '/Users/baldez/Desktop/JavaScript-examples/indexedDB-226.html'
+        ]);
+      })
+    );
+});
+   
+self.addEventListener('fetch', e => {
+    console.log(e.request.url);
+    e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+    );
+});
+   
